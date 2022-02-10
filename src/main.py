8c7ad54bbdde0a6a805d48e67a3ee50619569e25
张书宁 <zhang-sn19@mails.tsinghua.edu.cn>
2022-02-10 17:40:35 +0800

    [MODIFY]: Add data augmentation

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import ipdb
+import time
 import copy
 import sklearn
 from sklearn import svm
@@ -6,7 +7,7 @@ from sklearn.model_selection import train_test_split
 import numpy as np
 from sklearn.utils import column_or_1d
 from data_process import further_process
-from config import PADDING_LENGTH, INNER_PADDING
+from config import PADDING_LENGTH, INNER_PADDING, SNR, AUGMENT
 
 global_counter = 0
 def padding(input_list):
@@ -37,44 +38,93 @@ def padding(input_list):
     global_counter += 1
     return new_input_list
 
-single_list, double_list, single_list_labels, double_list_labels = further_process()
-new_single_list, new_double_list = [], [] 
-for single_element in single_list:
-    new_single_list += padding(single_element)
-for double_element in double_list:
-    new_double_list += padding(double_element)
-single_list, double_list = new_single_list, new_double_list 
-single_np_array = np.array(single_list)
-double_np_array = np.array(double_list)
+def _add_noise(element, snr):
+    snr = 10 ** (snr / 10.0)
+    xpower = np.sum(element ** 2) / len(element)
+    npower = xpower / snr
+    return np.array(np.random.randn(len(element)) * np.sqrt(npower)) + element
 
-data = np.concatenate((single_np_array, double_np_array), axis=0)
+def calculate_max(single_list, double_list):
+    return max(
+        max([len(single_list[_]) for _ in range(len(single_list))]),
+        max([len(double_list[_]) for _ in range(len(double_list))])
+    )
 
-x, y = np.split(data, (PADDING_LENGTH,), axis=1)
+def add_noise(max_groups, new_single_list, new_double_list):
+    ret_single_list, ret_double_list = [], []
+    for single_element in new_single_list:
+        noise_group_num = max_groups - len(single_element)
+        for single_ in single_element:
+            ret_single_list.append(np.array(single_))
+        for idx in range(0, noise_group_num):
+            ret_single_list.append(np.append(
+                    _add_noise(np.array(single_element[idx % len(single_element)][:-1]), SNR),
+                    np.array(single_element[idx % len(single_element)][-1])
+                ))
 
-# x = x[:, :2]
-# TODO: data augmentation 
-# x_after_aug, y_after_aug = augment(x, y)
-x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1, train_size=0.8, shuffle=True)
-y_train = column_or_1d(y_train, warn=True)
-y_test = column_or_1d(y_test, warn=True)
-# Assumed you have, X (predictor) and y (target) for training data set and x_test(predictor) of test_dataset
-# Create SVM classification object 
-model = svm.SVC(kernel='rbf', C=3, gamma='auto', decision_function_shape="ovr", tol=1e-5, verbose=1) 
-# There is various option associated with it, like changing kernel, gamma and C value. Will discuss more # about it in next section.Train the model using the training sets and check score
-model.fit(x_train, y_train)
-acc=model.score(x_train, y_train)
-print("\n--------------------------------\n")
-print(acc)
-print("--------------------------------\n")
-# Predict Output
-predicted = model.predict(x_test)
-total_count, correct_count = len(predicted), 0
+    for double_element in new_double_list: 
+        noise_group_num = max_groups - len(double_element)
+        for double_ in double_element:
+            ret_double_list.append(np.array(double_))
+        for idx in range(0, noise_group_num):
+            ret_double_list.append(np.append(
+                    _add_noise(np.array(double_element[idx % len(double_element)][:-1]), SNR),
+                    np.array(double_element[idx % len(double_element)][-1])
+                ))
 
-for element_index in range(len(predicted)):
-    correct_count += (predicted[element_index] == y_test[element_index])
+    return ret_single_list, ret_double_list
 
-print("--------------------------------\n")
-print("Acc: ", float(correct_count / total_count), "\n")
-print(predicted)
-print("\n")
-print("--------------------------------\n")
+def main_flow():
+    time_start_preparing = time.time()
+    single_list, double_list, single_list_labels, double_list_labels = further_process()
+    max_groups = calculate_max(single_list, double_list)
+    new_single_list, new_double_list = [], [] 
+    for single_element in single_list:
+        new_single_list.append(padding(single_element))
+    for double_element in double_list:
+        new_double_list.append(padding(double_element))
+    single_list, double_list = add_noise(max_groups * AUGMENT, new_single_list, new_double_list)
+    single_np_array = np.array(single_list)
+    double_np_array = np.array(double_list)
+
+    data = np.concatenate((single_np_array, double_np_array), axis=0)
+
+    x, y = np.split(data, (PADDING_LENGTH,), axis=1)
+
+    # x = x[:, :2]
+    # TODO: data augmentation 
+    # x_after_aug, y_after_aug = augment(x, y)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, random_state=1, train_size=0.8, shuffle=True)
+    y_train = column_or_1d(y_train, warn=True)
+    y_test = column_or_1d(y_test, warn=True)
+    time_end_preparing = time.time()
+    # Assumed you have, X (predictor) and y (target) for training data set and x_test(predictor) of test_dataset
+    # Create SVM classification object 
+    model = svm.SVC(kernel='rbf', C=3, gamma='auto', decision_function_shape="ovr", tol=1e-5) # , verbose=1
+    # There is various option associated with it, like changing kernel, gamma and C value. Will discuss more # about it in next section.Train the model using the training sets and check score
+    time_start_training = time.time()
+    model.fit(x_train, y_train)
+    acc=model.score(x_train, y_train)
+    time_end_training = time.time()
+    # Predict Output
+    time_start_predicting = time.time()
+    predicted = model.predict(x_test)
+    time_end_predicting = time.time()
+    total_count, correct_count = len(predicted), 0
+
+    for element_index in range(len(predicted)):
+        correct_count += (predicted[element_index] == y_test[element_index])
+
+    print("--------------------------------\n")
+    print("Total test samples: ", total_count, "\n")
+    print("Training acc: ", acc, "\n")
+    print("Testing Acc: ", float(correct_count / total_count), "\n")
+    print("Training time: ", (time_end_training - time_start_training), "\n")
+    print("Preparing time: ", (time_end_preparing - time_start_preparing), "\n")
+    print("Testing time: ", (time_end_predicting - time_start_predicting), "\n")
+    print("Predicted result: ", predicted, "\n")
+    print("\n")
+    print("--------------------------------\n")
+
+# Entrance
+main_flow()
